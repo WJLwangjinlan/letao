105d279ac0f510fa901c8714d8e7f5207a2975bb
WJLwangjinlan <18310689172@163.com>
2018-10-11 11:47:46 +0800

    分类管理.商品管理完成

--- js/common.js
+++ js/common.js
@@ -6,3 +6,17 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	
+     //检测后台管理员是否登录 
+    $.ajax({
+        url: '/api/employee/checkRootLogin',
+        type: 'get',           
+        success: function (info) {
+            console.log(info);
+            if (info.error) {
+                location.href = './login.html';
+            }
+
+        }
+    });
